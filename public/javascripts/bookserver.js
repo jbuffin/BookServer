bc163ca2bd5e257c8c9b86313f9fea72f92856b0
Jason Buffin <jason@loverofmysoul.com>
2013-10-16 22:19:33 -0400

    made the currentChapter object with observable text and title

--- public/javascripts/bookserver.js
+++ public/javascripts/bookserver.js
@@ -1,11 +1,19 @@
-var booksViewModel = {
-};
+var booksViewModel = {};
 if (ko) {
+	var ChapterObservable = {
+		init : function(chapterJSON) {
+			this.original = chapterJSON;
+			this.title = ko.observable(chapterJSON.title);
+			this.text = ko.observable(chapterJSON.text);
+		}
+	};
 	booksViewModel.init = function() {
 		this.getAllBooks();
 	};
 	booksViewModel.currentPage = ko.observable('chapters');
-	booksViewModel.allBooks = ko.observableArray([{'title':'Loading...'}]);
+	booksViewModel.allBooks = ko.observableArray([ {
+		'title' : 'Loading...'
+	} ]);
 	booksViewModel.currentBook = ko.observable();
 	booksViewModel.currentBookChapters = ko.observableArray([]);
 	booksViewModel.currentChapter = ko.observable();
@@ -19,13 +27,29 @@ if (ko) {
 		booksViewModel.getAllChapters(newValue._id.$oid)
 	});
 	booksViewModel.init();
-	ko.applyBindings(booksViewModel);
+	booksViewModel.setCurrentChapter = function(newChapter) {
+		var chapterObservable = Object.create(ChapterObservable);
+		chapterObservable.init(newChapter);
+		booksViewModel.currentChapter(chapterObservable);
+	};
+	$(function(){
+		ko.applyBindings(booksViewModel);		
+	});
 	booksViewModel.getAllChapters = function(bookId) {
 		jsRoutes.controllers.Books.getAllChapters(bookId).ajax({
 			success : this.currentBookChapters
 		});
 	};
-	booksViewModel.getChapter = function(bookId, chapterId) {
-		
+	ko.bindingHandlers.editableText = {
+		init : function(element, valueAccessor) {
+			$(element).on('blur', function() {
+				var observable = valueAccessor();
+				observable($(this).text());
+			});
+		},
+		update : function(element, valueAccessor) {
+			var value = ko.utils.unwrapObservable(valueAccessor());
+			$(element).text(value);
+		}
 	};
 }
